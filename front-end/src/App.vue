19b70c637f2fa69702829d7d8eee09b63ed839ce
Jackson Small <jackson40tall@gmail.com>
2020-12-03 14:29:27 -0700

    Nav bar and footer

---
+++ front-end/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <div id="content">
+      <div id="nav">
+        <div class="logo">
+          <router-link to="/">MeetUp</router-link>
+        </div>
+        <div class="pages">
+          <router-link to="/add">+ Activity</router-link>
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
+      <router-view />
+    </div>
+    <div class="footer">
+      <a href="">Github Repo</a>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #fcf8ec;
+  margin: 0px;
+  padding: 0px;
+}
+
+a {
+  text-decoration: none;
+}
+
+#nav {
+  padding: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: #d0e8f2;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav .pages a{
+  padding: 20px;
+}
+
+#content {
+  min-height: 100vh;
+}
+
+.footer {
+  background-color: #456268;
+  padding: 10px;
+}
+
+.footer a {
+  color: #fcf8ec;
+}
+
+</style>
